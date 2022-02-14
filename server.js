e044de396f3c1d857ee01376e8ca4781135ad377
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-13 22:18:02 -0700

    basic idea

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
 
-app.get(/.+/, (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('meow')
+})
+
+app.get('/dogs', (req,res) => {
+    res.send('rufff')
 })
 
 app.listen(3000, () => {
