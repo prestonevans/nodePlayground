2cc04918a97167f87f78617005aba41214eee358
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-16 20:59:38 -0700

    serving static files

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.get('/cats', (req, res) => {
-    res.send('meow')
-})
+	res.send('meow');
+});
 
-app.get('/dogs', (req,res) => {
-    res.send('rufff')
-})
+app.get('/dogs', (req, res) => {
+	res.send('rufff');
+});
+
+app.get('/chickens/:posts', (req, res) => {
+	const post = req.params.posts;
+	res.send(`Welcome to chickens/${post}`);
+});
+
+app.get('/search', (req, res) => {
+	const { q } = req.query;
+	res.send(`Results for ${q}`);
+});
+
+app.get(/.*/, (req, res) => {
+	res.send('Ooops nothing here with that page number');
+});
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+	console.log('listening on port 3000');
+});
